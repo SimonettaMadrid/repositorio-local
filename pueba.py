962e35fa7e047495c9720a0c9741311965cf5618
SimonettaMadrid <a01736976@tec.mx>
2025-10-02 18:25:28 -0600

    si

--- pueba.py
+++ pueba.py
@@ -6,3 +6,37 @@ print('Otra linea de codigo')
 print('hola')
 print('lol')
 print('lol')
+print("hola")
+
+print("mundo")
+
+for i in range (8):
+    print(i)
+
+a=2
+b=3
+c=5
+
+def suma():
+    print("suma", a+b+c)
+suma()
+
+# --- IGNORE ---
+def generar_datos_ventas ():
+    dias= ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+    ventas= [150, 200, 250, 300, 350, 400, 450]
+    return dias, ventas
+    return datos_ventas
+def resumen_analitica():
+    dias, ventas= generar_datos_ventas()
+    total= sum(ventas)
+    promedio= total/ len(ventas)
+    return{
+        "dias": dias,
+        "ventas": ventas,
+        "total": total,
+        "promedio": promedio
+    }
+datos=resumen_analitica()
+print(datos["total"])
+print(datos["promedio"])
